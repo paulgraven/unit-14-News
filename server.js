9cc1fb38a4c16fc58a1308849b71f7ecb93b11bf
Paul Graven <graven.paul@gmail.com>
2019-10-28 17:25:29 -0600

    updated .env for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
-
+require("dotenv").config();
 // Require all models
 var db = require("./models");
 
